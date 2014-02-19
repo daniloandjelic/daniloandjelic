fc545c83ecb28982b8d1e3370dc4ee3f181519f0
Danilo1989 <daniloandj@gmail.com>
2014-02-19 20:55:42 +0100

    codeplex code bugfix

---
+++ Client/ViewModel/FizickoLiceListViewModel.cs
@@ -0,0 +1,81 @@
+﻿using BusinessLogicLayer;
+using BusinessLogicLayer.Implementations;
+using Client.Commands;
+using Client.Framework;
+using Client.Windows;
+using DataAccessLayer;
+using MasterEntities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client.ViewModel
+{
+    public class FizickoLiceListViewModel : CollectionViewModel, IPageViewModel
+    {
+        public string Name
+        {
+            get { return "Fizicko lice"; }
+        }
+
+        public FizickoLiceListViewModel()
+        {
+            RefreshAllFizickaLica();            
+        }
+
+        public override void NewCommand_Execute(object obj)
+        {
+            NewWindow nv = new NewWindow(new FizickoLice(), obj as Window);
+            nv.Show();
+            AfterWindowClosing(nv, obj as Window);
+        }
+
+        public override void DeleteCommand_Execute(object obj)
+        {
+            MessageBoxResult result = MessageBox.Show(deleteMessage, "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                IBusinessLayerFacade<FizickoLice> flDelete = new FizickoLiceBusinessLayerImplementation();
+                flDelete.Delete(obj as FizickoLice);
+            }
+            this.RefreshCollectionOnPage();
+        }
+
+        public override void UpdateCommand_Execute(object obj)
+        {
+            OsobaViewModel ovm = obj as OsobaViewModel;
+            if (ovm != null && SelectedItem != null && ovm._mainWindow != null)
+            {
+                NewWindow nv = new NewWindow(SelectedItem, ovm._mainWindow);
+                nv.Show();
+                AfterWindowClosing(nv, ovm._mainWindow as Window);
+            }
+        }
+
+        private void RefreshAllFizickaLica()
+        {
+            CollectionItems = GetAllFizickaLica();
+        }
+
+
+
+        private ObservableCollection<object> GetAllFizickaLica()
+        {
+            GenericDataAccessLayer<FizickoLice> dal = new GenericDataAccessLayer<FizickoLice>();
+            List<FizickoLice> collFl = dal.GetAll().ToList();
+            ObservableCollection<object> oColl = new ObservableCollection<object>();
+            collFl.ForEach(c => oColl.Add(c));
+            return oColl;
+        }
+
+        public override void RefreshCollectionOnPage()
+        {
+            RefreshAllFizickaLica();
+        }
+
+    }
+}
---
+++ Client/ViewModel/PravnoLiceListViewModel.cs
@@ -0,0 +1,80 @@
+﻿using BusinessLogicLayer;
+using BusinessLogicLayer.Implementations;
+using Client.Commands;
+using Client.Framework;
+using Client.Windows;
+using DataAccessLayer;
+using MasterEntities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client.ViewModel
+{
+    public class PravnoLiceListViewModel : CollectionViewModel, IPageViewModel
+    {
+        public PravnoLiceListViewModel()
+        {
+            RefreshPravnaLica();
+        }
+
+        public override void NewCommand_Execute(object obj)
+        {
+            NewWindow nv = new NewWindow(new PravnoLice(), obj as Window);
+            nv.Show();
+            AfterWindowClosing(nv, obj as Window);
+        }
+
+        public override void DeleteCommand_Execute(object obj)
+        {
+            MessageBoxResult result = MessageBox.Show(deleteMessage, "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                IBusinessLayerFacade<PravnoLice> flDelete = new PravnoLiceBusinessLayerImplementation();
+                flDelete.Delete(obj as PravnoLice);
+            }
+            this.RefreshCollectionOnPage();
+        }
+
+        public override void UpdateCommand_Execute(object obj)
+        {
+            OsobaViewModel ovm = obj as OsobaViewModel;
+            if (ovm != null && SelectedItem != null && ovm._mainWindow != null)
+            {
+                NewWindow nv = new NewWindow(SelectedItem, ovm._mainWindow);
+                nv.Show();
+                AfterWindowClosing(nv, ovm._mainWindow as Window);
+            }
+        }
+
+
+        private void RefreshPravnaLica()
+        {
+            CollectionItems = GetAllPravnaLica();
+        }
+
+        public string Name
+        {
+            get { return "Pravna lica"; }
+        }
+
+
+        private ObservableCollection<object> GetAllPravnaLica()
+        {
+            GenericDataAccessLayer<PravnoLice> dal = new GenericDataAccessLayer<PravnoLice>();
+            List<PravnoLice> collFl = dal.GetAll().ToList();
+            ObservableCollection<object> oColl = new ObservableCollection<object>();
+            collFl.ForEach(c => oColl.Add(c));
+            return oColl;
+        }
+
+        public override void RefreshCollectionOnPage()
+        {
+            RefreshPravnaLica();
+        }
+    }
+}
